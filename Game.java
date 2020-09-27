8a5821b92152a428ac8213eadc6cfdbe2048d3ac
ClassicMike48 <71788173+ClassicMike48@users.noreply.github.com>
2020-09-27 14:37:54 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,47 @@
+
+public class Game{
+private String name;
+private double timePlayed;
+private double completion;
+private double personalRating;
+
+public Game(String name, double timePlayed, double completion, double personalRating) {
+  this.name = name;
+  this.timePlayed = timePlayed;
+  this.completion = completion;
+  this.personalRating = personalRating;
+}
+
+public String getName() {
+  return name;
+}
+
+public void setName(String name) {
+  this.name = name;
+}
+
+public void setTimePlayed(double timePlayed) {
+  this.timePlayed = timePlayed;
+}
+
+public void setCompletion(double completion) {
+  this.completion = completion;
+}
+
+public void setPersonalRating(double personalRating) {
+  this.personalRating = personalRating;
+}
+
+public double getTimePlayed() {
+  return timePlayed;
+}
+
+public double getCompletion() {
+   return completion;
+}
+
+public double getPersonalRating() {
+    return personalRating;
+}
+  
+}
