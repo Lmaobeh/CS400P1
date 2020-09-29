4ca516588e2f7a198dfe6363f6d8f7078ec4d82b
Lucas Nguyen <53918781+Lmaobeh@users.noreply.github.com>
2020-09-29 13:48:53 -0500

    Add files via upload

--- GameStorageADT.java
+++ GameStorageADT.java
@@ -1,15 +1,69 @@
 import java.util.NoSuchElementException;
 
+/**
+ * @author Lucas
+ *
+ * @param <KeyType>
+ * @param <ValueType>
+ */
 public interface GameStorageADT<KeyType,ValueType>{
 
+    /**
+     * Adds a value (with a key field) into the hash table.
+     * @param value the value to add
+     * @return returns true if added and false if otherwise
+     */
     public boolean add(ValueType value);
+    /**
+     * Searches the hash table for the given key for its corresponding value
+     * @param key the key to lookup
+     * @return the value of the key value pair
+     * @throws NoSuchElementException if the key was not found
+     */
     public ValueType lookup(KeyType key) throws NoSuchElementException;
+    /**
+     * returns size of table
+     * @return the size of table
+     */
     public int size();
+    /**
+     * Looks to see if the hash table contains a given key value pair
+     * @param key the key
+     * @return true if it does and false if otherwise
+     */
     public boolean containsKey(KeyType key);
+    /**
+     * Removes the given key value pair form the hash table
+     * @param key the key
+     * @return the value that was removed
+     */
     public ValueType remove(KeyType key);
+    /**
+     * Clears the hash table
+     */
     public void clear();
+	/**
+	 * Gets the game stats for a user
+	 * @param userName the user to get game stats for
+	 * @return the string of game stats
+	 * @throws NoSuchElementException if the user was not found
+	 */
 	public String[] getGameStats(String userName) throws NoSuchElementException;
+	/**
+	 * Returns a string of the game stats for a given user
+	 * @param userName the user game stats
+	 * @param gameName the game to get
+	 * @return a string of the game stats
+	 * @throws NoSuchElementException if the user or game doesnt exist
+	 */
 	public String getGameStats(String userName, String gameName) throws NoSuchElementException; 
-
-	
+	/**
+	   * Gets the game object from a user.
+	   * @param username the key for the user
+	   * @param gameName the name of the game
+	   * @return The game object.
+	   * @throws NoSuchElementException if the game doesnt exist
+	 */
+	public Game getGame(String userName, String gameName) throws NoSuchElementException;
+ 	
 }
--- GameStorageHashTable.java
+++ GameStorageHashTable.java
@@ -11,19 +11,31 @@ import java.util.NoSuchElementException;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * @author Lucas A class to store the users of the game storage aplications
+ *
+ */
 public class GameStorageHashTable implements GameStorageADT<String, User> {
   private LinkedList<User>[] hashTable;
   private int capacity;
   private int size;
   private final static double LOAD_THRESHOLD = .8;
-  private final static int GAME_NUMBER=10;
+  private final static int GAME_NUMBER = 10;
 
+  /**
+   * Initalizes hash table with a specified capacity
+   * 
+   * @param capacity the capacity
+   */
   public GameStorageHashTable(int capacity) {
     this.capacity = capacity;
     hashTable = (LinkedList<User>[]) new LinkedList[this.capacity];
     this.size = 0;
   }
 
+  /**
+   * Initalizes with a default 10
+   */
   public GameStorageHashTable() {
     this.capacity = 10;
     hashTable = (LinkedList<User>[]) new LinkedList[this.capacity];
@@ -39,10 +51,19 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     return (double) size / (double) capacity >= LOAD_THRESHOLD;
   }
 
+  /**
+   * Computes a hash index from a given key
+   * 
+   * @param key the key
+   * @return the hash index returned
+   */
   private int hashFunction(String key) {
     return (key.hashCode() & 0x7fffffff) % capacity;
   }
 
+  /**
+   * Preforms a resize operation on the hash table, doubling the capacity
+   */
   private void resizeTable() {
     LinkedList<User>[] temp = hashTable;
     hashTable = (LinkedList<User>[]) new LinkedList[capacity *= 2];
@@ -57,6 +78,10 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
 
   }
 
+  /**
+   * @param user Takes a user object to add to this table
+   * @return true if the user was successfully added, false if the user already existed
+   */
   @Override
   public boolean add(User value) {
     String key = value.getUsername();
@@ -64,8 +89,7 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
       return false;
     if (hashTable[hashFunction(key)] != null) {
       hashTable[hashFunction(key)].add(value);
-    }
-    else {
+    } else {
       hashTable[hashFunction(key)] = new LinkedList<User>();
       hashTable[hashFunction(key)].add(value);
     }
@@ -75,6 +99,13 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     return true;
   }
 
+  /**
+   * looks up a user in the hash table based on their key
+   * 
+   * @param key the key of the user to look up
+   * @return The user with the give key
+   * @throws NoSuchElementException if the user is no there
+   */
   @Override
   public User lookup(String key) throws NoSuchElementException {
     if (hashTable[hashFunction(key)] == null) {
@@ -88,11 +119,20 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     throw new NoSuchElementException();
   }
 
+  /**
+   * @returns the size
+   */
   @Override
   public int size() {
     return size;
   }
 
+  /**
+   * Looks to see if the corresponding user is in the hash table
+   * 
+   * @param String key key of user to check
+   * @return true if they are, false if otherwise
+   */
   @Override
   public boolean containsKey(String key) {
     if (hashTable[hashFunction(key)] == null) {
@@ -106,6 +146,12 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     return false;
   }
 
+  /**
+   * Removes a key value pair from the following hash table
+   * 
+   * @param Strin key the key of the user to remove
+   * @return the user or null if the user wasnt there
+   */
   @Override
   public User remove(String key) {
     if (this.containsKey(key)) {
@@ -113,8 +159,8 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
         if (node.getUsername().equals(key)) {
           size--;
           hashTable[hashFunction(key)].remove(node);
-          if(hashTable[hashFunction(key)].size() ==0) {
-            hashTable[hashFunction(key)]= null;
+          if (hashTable[hashFunction(key)].size() == 0) {
+            hashTable[hashFunction(key)] = null;
           }
           return node;
         }
@@ -123,31 +169,57 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     return null;
   }
 
+  /**
+   * Clears the hash table
+   */
   @Override
   public void clear() {
     hashTable = (LinkedList<User>[]) new LinkedList[this.capacity];
     size = 0;
-  } 
-  
+  }
+
+  /**
+   * @param username Users game stats to get
+   * @return a string of game stats for a user
+   * @throws NoSuchElementException if the user doenst exist
+   */
   public String[] getGameStats(String username) throws NoSuchElementException {
     Game[] gamelist = this.lookup(username).getGameList();
     String[] str = new String[gamelist.length];
     for (int i = 0; i < gamelist.length; i++) {
-     if(gamelist[i]== null) continue;
-     str[i] = gamelist[i].toString();
+      if (gamelist[i] == null)
+        continue;
+      str[i] = gamelist[i].toString();
     }
-   
+
     return str;
   }
-  
+
+  /**
+   * Gets the stats for a specific users game
+   * 
+   * @param username the name of the user
+   * @param gameName the name of the games states to get
+   * @return a string of the game object chosen
+   * @throws NoSuchElementException if the user doesnt exist
+   */
   public String getGameStats(String username, String gameName) throws NoSuchElementException {
     return getGame(username, gameName).toString();
   }
-  
-  public Game getGame(String username, String gameName) throws NoSuchElementException{
+
+  /**
+   * Gets the game object from a user.
+   * 
+   * @param username the key for the user
+   * @param gameName the name of the game
+   * @return The game object.
+   * @throws NoSuchElementException if the game doesnt exist
+   */
+  public Game getGame(String username, String gameName) throws NoSuchElementException {
     Game[] gamelist = this.lookup(username).getGameList();
     for (int i = 0; i < gamelist.length; i++) {
-      if (gamelist[i]==null ) continue;
+      if (gamelist[i] == null)
+        continue;
       if (gamelist[i].getName().equalsIgnoreCase(gameName)) {
         return gamelist[i];
       }
@@ -158,7 +230,7 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
   public static void main(String[] args) {
     Game[] gl = new Game[10];
     gl[0] = new Game("DOOM", 0.0, 0.0, 0.0);
-    gl[1] = new Game("Destiny", 0.0,0.0, 0.0);
+    gl[1] = new Game("Destiny", 0.0, 0.0, 0.0);
     GameStorageHashTable table = new GameStorageHashTable();
     table.add(new User("Lucas", gl));
     System.out.println(table.size());
@@ -175,14 +247,14 @@ public class GameStorageHashTable implements GameStorageADT<String, User> {
     table.add(new User("kakooooooo", null));
     System.out.println(table.capacity);
     System.out.println(table.lookup("kakooooo"));
-    //System.out.println(table.get("hello"));
+    // System.out.println(table.get("hello"));
     System.out.println(table.remove("kakooooo"));
     System.out.println(table.size);
     table.add(new User("Lucas", gl));
     System.out.println(Arrays.toString(table.getGameStats("Lucas")));
     System.out.println(table.getGameStats("Lucas", "DOOM"));
     System.out.println(table.getGameStats("Lucas", "GameDoesntexist"));
-    
+
   }
 
 }
