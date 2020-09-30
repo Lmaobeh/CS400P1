c824f0b40e76176ea1a32785a6ead8cd7a26afde
StealthyShadow <71903286+StealthyShadow@users.noreply.github.com>
2020-09-29 19:48:11 -0500

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -1,6 +1,6 @@
 // --== CS400 File Header Information ==--
-// Name: Qosai Kadadha, Lucas Nguyen
-// Email: Kadadha@wisc.edu, lfnguyen@wisc.edu
+// Name: Qosai
+// Email: Kadadha@wisc.edu
 // Team: NE
 // TA: Daniel Finer
 // Lecturer: Florian Heimerl 
@@ -29,23 +29,31 @@ public class Commands {
      if (newScore < 0.0 || newScore > 5.0) throw new IndexOutOfBoundsException();
      table.getGame(user, game).setPersonalRating(newScore);
    }
-  
-  
+  //checks input file and puts all data inside dataCollector variable that can later be put into table
+  public static DataCollector updateDataCollector(DataCollector d, GameStorageHashTable table) {
+    try {
+      d = new DataCollector("Input.txt"); 
+      d.updateInput();
+      return d;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+  //updates table with data from input file
+    public static GameStorageHashTable updateTable(DataCollector d,GameStorageHashTable table) {
+    for (int i = 0; i < d.getUsers().length; i++) {
+      table.add(d.getUsers()[i]);
+    }
+    return table;
+  }
   public static void main(String[] args) {
     GameStorageHashTable table = new GameStorageHashTable(10);
     Scanner scnr = new Scanner(System.in);
     boolean run = true;
     DataCollector d = null;
-    try {
-      d = new DataCollector("Input.txt"); //adds text file to game array 
-      d.updateInput();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-    //adds users to array
-    for (int i = 0; i < d.getUsers().length; i++) {
-      table.add(d.getUsers()[i]);
-    }
+    d = updateDataCollector(d,table);
+    table = updateTable(d,table);
     Game[] emptyGameList = d.emptyStorage();
     System.out.println("Welcome to the Video Game Database, please enter a username");
     User user1 = new User(scnr.nextLine(), emptyGameList);//change later
@@ -56,28 +64,26 @@ public class Commands {
     System.out.println("Enter p to view all game data \nEnter t to change time played in a game \n"
         + "Enter c to change completion percentage \nEnter s to change personal rating \nEnter u to switch users"
         + "\nEnter r to remove a user \nEnter x to end program");
+    try {
     switch(scnr.next()){        //switch statement runs through all possible commands 
       case "p": printAll(table, user1.getUsername()); 
         break;
       case "t": System.out.println("Enter the new amount of time played");
       double newTime = scnr.nextDouble();
-        scnr.nextLine();
         System.out.println("Enter the name of the game you'd like to change");
-        String game = scnr.nextLine();
+        String game = scnr.next();
         changeTime(table, newTime, user1.getUsername(),game);
         break;
       case "c": System.out.println("Enter the new amount of percentage completed");
         double newPercent = scnr.nextDouble();
         System.out.println("Enter the name of the game you'd like to change");
-        scnr.nextLine();
-        String game2 = scnr.nextLine();
+        String game2 = scnr.next();
         changePercentage(table, newPercent, user1.getUsername(),game2);
         break;
       case "s": System.out.println("Enter the new personal score");
         double newScore = scnr.nextDouble();
         System.out.println("Enter the name of the game you'd like to change");
-        scnr.nextLine();
-        String game3 = scnr.nextLine();
+        String game3 = scnr.next();
         changeScore(table, newScore, user1.getUsername(),game3);
         break;
       case "u":
@@ -92,6 +98,10 @@ public class Commands {
       case "x": run = false;
       break;
       default: System.out.println("Please enter a valid command");
+    }
+    }
+    catch(IndexOutOfBoundsException e) {
+      System.out.println("you have put in an invalid number, please try again");
     }
    }
     System.out.println("Thank you for using the video game database");
